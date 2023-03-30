e41b448fe5a66c76bf550b63b3c3c3cdf74e2c11
wyclif-maitho <isaacmaitho14@gmail.com>
2023-03-30 23:01:58 +0300

    a new solution using multiple pointers

--- Example_4.js
+++ Example_4.js
@@ -13,3 +13,21 @@ function sumZero(arr){
         }
     }
 }
+
+//using another approach(multiple pointers)
+//time complexity O(N) space complexity O(1)
+ function sumZero(arr){
+    let left =0;
+    let right= arr.length-1;
+
+    while(left<right){
+        let sum = arr[left] +arr[right]
+        if(sum ===0){
+            return [arr[left], arr[right]]
+        }else if(sum>0){
+            right--;
+        }else{
+            left++;
+        }
+    }
+ }
