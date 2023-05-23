a8c5c0ae3e1e28d5424d3153e2894f85cdd4d579
wyclif-maitho <isaacmaitho14@gmail.com>
2023-05-23 11:27:35 +0300

    Added oddValues function

---
+++ OddValues/helperFunction.js
@@ -0,0 +1,15 @@
+function oddValues(arr){
+    let result = []
+    function helper(input){
+     if(input.length === 0){
+        return;
+    }
+
+     if(input[0] % 2 !== 0){
+        return result.push(input[0])
+    }
+    helper(input.slice(1))
+  }
+  helper(arr)
+  return result
+}
